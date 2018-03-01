876d049fe36c419e02f4257777d694ca63c4f940
j.pauliukevicius <jonas.pauliukevicius@adform.com>
2018-03-01 20:28:02 +0200

    some basic classes

---
+++ Rides/CarState.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Rides
+{
+    public class CarState
+    {
+        public int Id { get; set; }
+        public Point Location { get; set; }
+        public int CurrentTime { get; set; }
+    }
+
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int Distance(Point other)
+        {
+            return Math.Abs(X - other.X) + Math.Abs(Y - other.Y);
+        }
+    }
+
+    public class Ride
+    {
+        public Point From { get; set; }
+        public Point To { get; set; }
+
+        public int Pickup { get; set; }
+        public int Finish { get; set; }
+
+        public int WaitTime(CarState carState)
+        {
+            return Pickup - carState.CurrentTime - carState.Location.Distance(From);
+        }
+    }
+}
