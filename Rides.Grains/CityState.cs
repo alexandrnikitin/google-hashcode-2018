79d8b8d9a403909abdf74e1ac253529aaa1652ec
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2018-03-07 20:30:49 +0200

    ser

--- Rides.Grains/CityState.cs
+++ Rides.Grains/CityState.cs
@@ -10,25 +10,25 @@ namespace Rides.Grains
     [Serializable]
     public class CityState : IState<MakeRideAction>
     {
-        private readonly ImmutableList<Car> _cars;
-        private readonly RidesView3 _rides;
+        public ImmutableList<Car> Cars { get; set; }
+        public RidesView3 Rides { get; set; }
         private double _score;
 
         public CityState(ImmutableList<Car> cars, RidesView3 rides)
         {
-            _cars = cars;
-            _rides = rides;
+            Cars = cars;
+            Rides = rides;
         }
 
         public IEnumerable<MakeRideAction> GetAvailableActions()
         {
-            if (_rides.Count == 0) return new List<MakeRideAction>(0);
-            var ride = _rides.GetEarliestFinish();
+            if (Rides.Count == 0) return new List<MakeRideAction>(0);
+            var ride = Rides.GetEarliestFinish();
             if (ride.Equals(default)) return new List<MakeRideAction>(0);
 
-            var actions = new List<MakeRideAction>(_cars.Count + 1);
+            var actions = new List<MakeRideAction>(Cars.Count + 1);
 
-            foreach (var car in _cars)
+            foreach (var car in Cars)
             {
                 if (car.CanMakeIt(ride))
                 {
@@ -43,10 +43,10 @@ namespace Rides.Grains
 
         public IState<MakeRideAction> Apply(MakeRideAction action)
         {
-            var newRides = _rides.Remove(action.Ride);
+            var newRides = Rides.Remove(action.Ride);
             if (action.Car.Equals(Car.SkipRide))
             {
-                return new CityState(_cars, newRides);
+                return new CityState(Cars, newRides);
             }
 
             _score += action.GetFactScore(0); // TODO bonus
@@ -61,7 +61,7 @@ namespace Rides.Grains
                     action.Car.Id,
                     new Point(action.Ride.To.X, action.Ride.To.Y),
                     action.Car.Time + totalRideTime);
-            var newCars = _cars.Replace(action.Car, newCar);
+            var newCars = Cars.Replace(action.Car, newCar);
             return new CityState(newCars, newRides);
 
         }
